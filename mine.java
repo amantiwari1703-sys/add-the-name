3c2d3da066638ae2022fd55520b599eaf7ee3f77
Aman Tiwari <amantiwari@gmail.com>
2026-04-04 18:11:03 +0530

    fuction

--- mine.java
+++ mine.java
@@ -3,9 +3,25 @@ public class mine{
         int a=4;
         switch (a){
             case 1:
-                S;
+                System.out.println("case is my");
+            case 2:
+                System.out.println("good");
+            case 3:
+                System.out.println("best");
+            case 4:
+                System.out.println("very nice");
 
         }
+        String str="aman";
+         switch (str){
+             case "aman":
+                 System.out.println("MCA");
+             case "mahesh":
+                 System.out.println("good name");
+             case "raju":
+                 System.out.println("Indian");
+
+         }
 
     }
 }

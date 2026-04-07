a33c9e5c1b797fdb30b05f27a05f46a5d0437e5f
Aman Tiwari <amantiwari@gmail.com>
2026-04-07 12:36:12 +0530

    new

---
+++ good.java
@@ -0,0 +1,16 @@
+public class good {
+    int a=5;
+    String name="aman";
+    public good(int b,String anuj){
+        a=b;
+        name=anuj;
+
+
+        }
+    public static void main(String args[]){
+        good am=new good(45,"bad boy");
+        System.out.println(am.a);
+        System.out.println(am.name);
+
+    }
+}

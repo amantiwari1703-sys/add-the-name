0663f11bc0a1713e244832e37e75c6714cf423d7
Aman Tiwari <amantiwari@gmail.com>
2026-04-07 13:21:01 +0530

    darr

---
+++ Darr.java
@@ -0,0 +1,20 @@
+public class Darr {
+    int b=30;
+    String f="ear";
+
+    public Darr(int d,String f){
+        this.b=d;
+        this.f=f;
+
+
+        }
+        public static void main (String args[]){
+        Darr en=new Darr(33,"border");
+            System.out.println(en.b);
+            System.out.println(en.f);
+
+        }
+
+
+    }
+

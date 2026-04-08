13d186b5bb18d189485cd6273aa251b6171780a6
Aman Tiwari <amantiwari@gmail.com>
2026-04-08 08:47:17 +0530

    student

---
+++ student.java
@@ -0,0 +1,12 @@
+public class student {
+    public student(){
+        System.out.println("constructor called");
+
+
+    }
+    public static void main(String args[]){
+        student s=new student();
+
+
+    }
+}

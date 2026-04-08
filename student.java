6bc918b05e0fa00d0cd28ba769605c269e492172
Aman Tiwari <amantiwari@gmail.com>
2026-04-08 08:57:18 +0530

    string

--- student.java
+++ student.java
@@ -1,11 +1,16 @@
 public class student {
-    public student(){
-        System.out.println("constructor called");
+    String name;
+
+    public student(String n){
+        name=n;
+
+//        System.out.println("constructor called");
 
 
     }
     public static void main(String args[]){
-        student s=new student();
+        student s=new student("Ram");
+        System.out.println(s.name);
 
 
     }

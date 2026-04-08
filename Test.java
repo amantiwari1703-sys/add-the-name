0d203ea62904c3a3573cb80c9ffb1c197e3b957f
Aman Tiwari <amantiwari@gmail.com>
2026-04-08 09:51:18 +0530

    test

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public Test(){
+        System.out.println("Hello constructor");
+
+    }
+    public static void main (String args[]){
+        Test t=new Test();
+
+    }
+}

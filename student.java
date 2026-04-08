f495044dee1b0b0691e1cac99df0b3e8b3a7961f
Aman Tiwari <amantiwari@gmail.com>
2026-04-08 09:31:12 +0530

    multiple object

--- student.java
+++ student.java
@@ -1,18 +1,28 @@
 public class student {
- String name;
-     int age;
-   public student(String n , int a){
-      name=n;
-    age=a;
-       System.out.println(age);
+    String name;
+
+    //     int age;
+    public student(String n) {
+        name = n;
+    }
+
+    void show() {
+        System.out.println(name);
+    }
+//    age=a;
+//       System.out.println(age);
 //        System.out.println("constructor called");
 
 
-    }
-    public static void main(String args[]){
-        student s=new student("Ram",21);
-        System.out.println(s.name);
+    public static void main(String args[]) {
+        student s1 = new student("Ram");
+        student s2 = new student("Shyam");
+//        System.out.println(s.name);
+
+        s1.show();
+        s2.show();
 
 
     }
 }
+

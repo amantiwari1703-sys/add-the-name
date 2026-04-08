1ec349e7b46e63a6d4060c4719ad7ccdd1a7e036
Aman Tiwari <amantiwari@gmail.com>
2026-04-08 09:59:45 +0530

    this

--- student.java
+++ student.java
@@ -1,28 +1,11 @@
 public class student {
-    student (){
-        System.out.println("default constructor");
-
-    }
-
-
-
-    //     int age;
-    public student(String name) {
-        System.out.println(name);
-    }
-
-//    void show() {
-//        System.out.println(name);
-//    }
-//    age=a;
-//       System.out.println(age);
-//        System.out.println("constructor called");
-
-
-    public static void main(String args[]) {
-        student s1 = new student();
-        student s2 = new student("Ram");
-//       System.out.println(s2.name);
+ String name;
+ public student(String name) {
+     this.name = name;
+ }
+ public static void main(String args[]) {
+     student n=new student("Ram");
+     System.out.println(n.name);
 
 
 

868c54c059fde1aa79c4fe2f947a1b78e325d752
Aman Tiwari <amantiwari@gmail.com>
2026-04-09 08:05:47 +0530

    default constructor

---
+++ rajat.java
@@ -0,0 +1,13 @@
+public class rajat {
+    String name;
+    public rajat(String n){
+//        System.out.println("constructor called");
+     name=n;
+    }
+    public static void main(String[] args){
+        rajat r=new rajat("Ram");
+        System.out.println(r.name);
+
+
+    }
+}

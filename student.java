8aad133a503079979357e04282a2557923a8f320
Aman Tiwari <amantiwari@gmail.com>
2026-04-09 09:32:01 +0530

    simple

--- student.java
+++ student.java
@@ -1,15 +1,17 @@
 public class student {
- String name;
- public student(String name) {
-    name=name;
- }
- public static void main(String args[]) {
-     student n=new student("Ram");
-     System.out.println(n.name);
+    String name;
+    int age;
+// public student(String name) {
+    // name=name;
 
-
+    public static void main(String args[]) {
+        student n = new student();
+        n.name = "Ram";
+        n.age = 20;
+        System.out.println(n.name);
+        System.out.println(n.age);
 
 
     }
-}
+   }
 

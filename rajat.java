0de8d7d8a6836a7bd9af8e39d0d45561dea320ce
Aman Tiwari <amantiwari@gmail.com>
2026-04-09 08:16:02 +0530

    multiple

--- rajat.java
+++ rajat.java
@@ -1,14 +1,15 @@
 public class rajat {
     int age ;
-//    String name;
-    public rajat(int a){
+   String name;
+    public rajat(int a,String n){
 //        System.out.println("constructor called");
-//     name=n;
+    name=n;
         age=a;
     }
     public static void main(String[] args){
-        rajat r=new rajat(47);
+        rajat r=new rajat(47,"Ram");
         System.out.println(r.age);
+        System.out.println(r.name);
 
 
     }
